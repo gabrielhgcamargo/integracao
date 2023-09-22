2f14ff114cf1c188c9cadd6b609177b77c9e56f0
Gabriel Henrique Garcia Camargo <gacamargos4@gmail.com>
2023-09-22 00:39:58 -0300

    feature: swagger

--- src/main/java/integracao/CursoController.java
+++ src/main/java/integracao/CursoController.java
@@ -1,22 +1,37 @@
 package integracao;
 
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/cursos")
+@Tag(name = "Curso Controller")
 public class CursoController {
     
     private List<Curso> cursosCadastrados = new ArrayList<>();
 
     @PostMapping
+    @Operation(summary = "Realiza o cadastro de cursos.", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cadastro de Curso realizado com sucesso."),
+            @ApiResponse(responseCode = "400", description = "Parametros inválidos."),
+    })
     public void adicionarCurso(@RequestBody Curso curso) {
         cursosCadastrados.add(curso);
     }
 
     @GetMapping
+    @Operation(summary = "Lista todos os cursos cadastrados", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cursos listados com sucesso"),
+    })
     public List<Curso> listarCursos() {
         return cursosCadastrados;
     }
--- src/main/java/integracao/IntegracaoApplication.java
+++ src/main/java/integracao/IntegracaoApplication.java
@@ -2,10 +2,15 @@ package integracao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Swagger de: Integracao", version = "1.0.0", description = "Documentação das rotas de GET/POST de Cursos."))
 public class IntegracaoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(IntegracaoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoApplication.class, args);
+	}
+
 }
