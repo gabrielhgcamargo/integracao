9a642f3a67120b70818fcc43ef12580f959bebe5
Gabriel Henrique Garcia Camargo <gacamargos4@gmail.com>
2023-09-29 10:39:16 -0300

    fix: remove unused atributes

--- src/main/java/integracao/Models/Aluno.java
+++ src/main/java/integracao/Models/Aluno.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class Aluno {
-	private int frequencia;
-	private float nota1;
-	private float nota2;
-	private float notaFinal;
+	private String nome;
+	private String cpf;
 	private String voucher;
 	private ArrayList<Curso> cursosConcluidos;
 	
@@ -24,64 +22,6 @@ public class Aluno {
 	
 	public String getVoucher() {
 		return voucher;
-	}
-
-	public int getFrequencia() {
-		return frequencia;
-	}
-
-	public void setFrequencia(int frequencia) {
-		this.frequencia = frequencia;
-	}
-
-	public float getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(float nota1) {
-		this.nota1 = nota1;
-	}
-
-	public float getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(float nota2) {
-		this.nota2 = nota2;
-	}
-
-	public void setNotaFinal(float notaFinal) {
-		this.notaFinal = notaFinal;
-	}
-
-	public boolean calcularAprovacao() {
-
-		float media;
-		if (frequencia < 75) {
-			return false;
-		}
-
-		else {
-			media = (nota1 + nota2) / 2;
-			if (media < 30) {
-				return false;
-			} else {
-				if (media >= 70) {
-					return true;
-				} else {
-					if ((media + notaFinal) / 2 >= 50) {
-						return true;
-					} else {
-
-						return false;
-
-					}
-
-				}
-
-			}
-
-		}
 	}
 
 	public boolean concluiuDozeCursos() {
--- src/test/java/integracao/AlunoTest.java
+++ src/test/java/integracao/AlunoTest.java
@@ -1,7 +1,5 @@
 package integracao;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,56 +10,7 @@ public class AlunoTest {
 	
 	Aluno aluno;
 	Curso curso;
-
-
-	@Test
-	public void testCalcularAprovacao_ReprovacaoFrequencia(){
-
-		aluno = new Aluno();
-		aluno.setFrequencia(74);
-		assertEquals(false, aluno.calcularAprovacao());
-	}
-
-	@Test
-	public void testCalcularAprovacao_ReprovacaoNota() {
-		aluno = new Aluno();
-		aluno.setFrequencia(75);
-		aluno.setNota1 (29);
-		aluno.setNota2 (30);
-		assertEquals(false, aluno.calcularAprovacao());
-	}
-
-	@Test
-	public void testAprovacao_AprovacaoNota(){
-		aluno = new Aluno();
-		aluno.setFrequencia(75);
-		aluno.setNota1 (70);
-		aluno.setNota2 (70);
-		assertEquals(true, aluno.calcularAprovacao());
-	}
-
-	@Test
-	public void testAprovacao_AprovacaoFinal(){
-		aluno = new Aluno();
-		aluno.setFrequencia(75);
-		aluno.setNota1 (30);
-		aluno.setNota2 (30);
-		aluno.setNotaFinal (70);
-		assertEquals(true, aluno.calcularAprovacao());
-	}
-
-	@Test
-	public void testCalcularAprovacao_ReprovacaoFinal(){
-		aluno = new Aluno();
-		aluno.setFrequencia(75);
-		aluno.setNota1 (30);
-		aluno.setNota2 (30);
-		aluno.setNotaFinal (69);
-		assertEquals(false, aluno.calcularAprovacao());
-
-
-	}
-
+	
 	   @Test
 	    public void testaAlunoNaoConcluiuDozeCursos() { // Teste do método -> AlunoConcluiuDozeCursos()
 		    aluno = new Aluno();
